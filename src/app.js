cf91b0c80e16f64f6ce12b31371c460d838a5293
kujakux <55299075+kujakux@users.noreply.github.com>
2020-07-13 18:29:34 -0500

    rutas del aside de doctor

--- src/app.js
+++ src/app.js
@@ -40,13 +40,12 @@ app.use(flash());//para enviar mensajes
 
 //Importar rutas
 const indexRoutes = require('./routes/index');
-const Perfil = require('./routes/clinica');
+require('./routes/clinica')(app);
 require('./routes/usuario')(app, passport)//se le envia passport a las rutas para poder trabajar la autenticacion en las mismas
 //require('./routes/cliente')(app, passport)
 
 // rutas del inicio
 app.use('/', indexRoutes);
-app.use('/Perfil',Perfil);
 
 
 /*
