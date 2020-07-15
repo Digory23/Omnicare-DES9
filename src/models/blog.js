a9e6f2cff5e241a6acee3c368651f4ff86ec5dd2
virgilio-v <virgiliovalentn@gmail.com>
2020-07-14 23:14:50 -0500

    Posts del blog

--- src/models/blog.js
+++ src/models/blog.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const blogSchema = new mongoose.Schema({
     titulo_post: String,
     cuerpo_post: String,
+    desc_post: String,
     fecha_post: Date,
     autor_post: String
 });
