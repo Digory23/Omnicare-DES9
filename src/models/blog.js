c96d0d32b9fe67cda6ff48a3b2b9757e63f75684
virgilio-v <virgiliovalentn@gmail.com>
2020-07-14 21:57:54 -0500

    blog

---
+++ src/models/blog.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+    titulo_post: String,
+    cuerpo_post: String,
+    fecha_post: Date,
+    autor_post: String
+});
+
+module.exports = mongoose.model('blog', blogSchema, 'blog');
